aea8cff82db471853b8d7e1a03e50a30aba353c2
wendellbarcelos <wendell_barcelos2@hotmail.com>
2021-01-26 05:46:16 -0300

    estilizando segunda pagina

---
+++ README.md
@@ -0,0 +1,34 @@
+## Sobre o projeto :computer:
+
+
+
+Projeto criado como estudo do bootcamp da rocketseat em **ReactJS**, **Typescript** e outras dependências criando um explorer de repositorios do github usando a api do proprio github. 🚀.
+
+
+
+### Tecnologias 🚀
+
+------
+
+#### :heavy_check_mark:HTML
+
+#### :heavy_check_mark:CSS
+
+#### :heavy_check_mark:JavaScript
+
+#### :heavy_check_mark:ReactJS
+
+#### :heavy_check_mark:TypeScript
+
+#### :heavy_check_mark:ESLint
+
+#### :heavy_check_mark:Prettier
+
+------
+
+#### Ferramentas
+
+##### [VS CODE](https://code.visualstudio.com/)
+
+[Git]()
+
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,67 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/23709704?s=460&u=c5dc98a6667e0c8155230c5dce512ddc282b8989&v=4"
+            alt="wendell barcelos"
+          />
+          <div>
+            <strong>Wendell Barcelos</strong>
+            <p>descrição do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1880</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="ashuahs">
+          <div>
+            <strong>ashuahsua</strong>
+            <p>ahsuhaus</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
